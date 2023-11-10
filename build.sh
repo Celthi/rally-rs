7b079c697c457254f296a2711e06a3dd0ad43e4a
EvanL00 <evan_l00@qq.com>
2023-11-10 20:48:08 +0800

    consume name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-version=2.1.9
+version=2.3.0
 cargo check && docker build --network=host . -t tnt:$version
 kubectl set image  deployment/tnt tnt=tnt:$version
--- src/msg/message.rs
+++ src/msg/message.rs
@@ -1,5 +1,11 @@
 use serde::{Deserialize, Serialize};
+use chrono::prelude::*;
 
+fn get_task_name(date: &DateTime<Utc>) -> String {
+    let s = "Code review ".to_string();
+    let date = date.format("%Y-%m-%d").to_string();
+    s + &date
+}
 #[derive(Deserialize, Serialize, Default, Clone)]
 pub struct TimeSpent {
     user: String,
@@ -10,6 +16,8 @@ pub struct TimeSpent {
     repo_name: Option<String>,
     pr_number: Option<u64>,
     pub task_name: Option<String>,
+    source: Option<String>,
+    text: Option<String>,
 }
 
 impl TimeSpent {
@@ -30,5 +38,46 @@ impl TimeSpent {
     }
     pub fn get_pr_number(&self) -> Option<u64> {
         self.pr_number
+    }
+    pub fn get_task_name(&self) -> String {
+        if self.task_name.is_some() {
+            return self.task_name.as_deref().unwrap().to_string();
+        }
+        match self.source {
+            Some(ref s) => {
+                if let Some(value) = self.get_task_name_from_source_and_text(s) {
+                    let mut name = self.user.clone();
+                    name.push_str(&value);
+                    return name;
+                }
+                "undefined".to_string()
+            }
+            None => "undefined".to_string(),
+        }
+    }
+
+    fn get_task_name_from_source_and_text(&self, s: &String) -> Option<String> {
+        if s == "github" {
+            return Some(get_task_name(&Utc::now()));
+        }
+        if s == "rally" {
+            if self.text.is_some() {
+                let text = self.text.as_deref().unwrap().to_lowercase();
+                if text.contains("root cause") {
+                    return Some("Root cause analysis".to_string());
+                }
+                if text.contains("Update") {
+                    return Some("Investigated the issue".to_string());
+                }
+            }
+            return Some("Review and support".to_string());
+        }
+        None
+    }
+    pub fn get_source(&self) -> Option<&str> {
+        self.source.as_deref()
+    }
+    pub fn get_text(&self) -> Option<&str> {
+        self.text.as_deref()
     }
 }
